549f95d10b8cf1f2c2bb36a4f79b36ce99b85e22
star <103299926+zhangxingyang@users.noreply.github.com>
2023-08-10 22:59:45 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import "../text/text.css";
-
 import { createApp } from "vue";
 import App from "./App.vue";
+import router from "./router";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
