a6b666ae278335278e8fa429369a3fa9af7ff472
“lemon” <3573045100@qq.com>
2023-08-13 15:03:09 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-    history: createWebHashHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: "/",
-            name: "index",
-            component: () => import("@/views/IndexView.vue"),
-        },
-        {
-            path: "/message",
-            name: "message",
-            component: () => import("@/views/MessageView.vue")
-        }
-    ],
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "index",
+      component: () => import("@/views/IndexView.vue"),
+    },
+    {
+      path: "/message",
+      name: "message",
+      component: () => import("@/views/MessageView.vue"),
+    },
+  ],
 });
 
 export default router;
