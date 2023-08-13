562e055d91264a8d2f9d1cb292cb60e46d11f576
star <1598624985@qq.com>
2023-08-13 10:28:22 +0800

    add vercel & use web history

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,18 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-    history: createWebHashHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: "/",
             name: "index",
             component: () => import("@/views/IndexView.vue"),
         },
+        {
+            path: "/message",
+            name: "message",
+            component: () => import("@/views/MessageView.vue")
+        }
     ],
 });
 
